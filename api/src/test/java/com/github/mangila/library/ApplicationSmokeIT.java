679ddf07cb0c34cb47df45d6d8bc799a8ac220f2
Erik Olsson <olsson.erik1993@gmail.com>
2026-07-26 11:14:28 +0200

    chore: configure integration tests, add SpotBugs plugin, and enhance HTTP settings

---
+++ api/src/test/java/com/github/mangila/library/ApplicationSmokeIT.java
@@ -0,0 +1,15 @@
+package com.github.mangila.library;
+
+import static io.restassured.RestAssured.given;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusIntegrationTest
+class ApplicationSmokeIT {
+
+  @Test
+  void smoke() {
+    given().when().get("/q/health/ready").then().statusCode(200);
+  }
+}
